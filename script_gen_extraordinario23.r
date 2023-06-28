28778f9430b117759a85e3d810aadcf6fa804145
Adrimgedu <109221366+Adrimgedu@users.noreply.github.com>
2023-06-28 22:21:37 +0200

    version jesus corregida

--- script_gen_extraordinario23.r
+++ script_gen_extraordinario23.r
@@ -1,10 +1,8 @@
+pck.list <- list('janitor','dplyr','tidyr')
+notinst.packages<-pck.list[!pck.list %in% installed.packages()]
+lapply(notinst.packages,install.packages,dependencies=TRUE)
+lapply(pck.list[!paste0('package:',pck.list)%in%search()],require,character.only=TRUE)
 
-install.packages("janitor",verbose = F, quiet = T)
-install.packages('dplyr',verbose = F, quiet = T) 
-install.packages('tidyr',verbose = F, quiet = T) 
-require(janitor)
-require(dplyr)
-require(tidyr)
 
 datos <- readr::read_csv(url("https://raw.githubusercontent.com/Adrimgedu/test/main/HDHI%20Admission%20data.csv"))
 
@@ -18,3 +16,4 @@ datos <- janitor::clean_names(datos)
 
 datos <- dplyr::select(.data = datos,sno,mrd_no,d_o_a,d_o_d,age,gender,rural,type_of_admission_emergency_opd,duration_of_stay,outcome,smoking,alcohol,hb,tlc,urea,creatinine,anaemia,heart_failure,pulmonary_embolism,chest_infection)
 datos <- tidyr::drop_na(datos) #Añado nombre.
+rm(list=ls()[!ls()%in% c('datos')])
